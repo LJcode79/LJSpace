2e6381406ac6cce71c8966a0563fbdc9d713136c
LJ <ljdb1999@gmail.com>
2025-01-26 16:24:36 -0800

    Add project files.

---
+++ LJSpace/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LJSpace.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
