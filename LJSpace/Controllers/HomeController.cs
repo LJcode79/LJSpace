46ef50395d22fb3de072aba8b9e66ef00c42e9ee
LJ <ljdb1999@gmail.com>
2025-02-04 23:44:31 -0800

    2/4/2025 v3
    started working on index page, signing out for tonight.

--- LJSpace/Controllers/HomeController.cs
+++ LJSpace/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace LJSpace.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
